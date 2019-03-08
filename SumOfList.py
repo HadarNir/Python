74362f9598256d3c60bcdfbe8236a53472bc899f
HadarNir <hadar4nir@gmail.com>
2019-03-08 12:45:02 +0200

    finish sum of list

--- SumOfList.py
+++ SumOfList.py
@@ -1,8 +1,23 @@
-
 def main():
     """
-    
+    the main ask for numbers to insert inside the list
     """
+    list_nums = []
+    input_from_user = input()
+    while input_from_user != "stop":
+        list_nums.append(input_from_user)
+        input_from_user = input()
+    sum_of_list(list_nums)
+
+
+def sum_of_list(l):
+    """the function calculate and print the sum of integers inside a list"""
+    sum_nums = 0
+    while l:
+        x = l[0]
+        del l[0]
+        sum_nums += int(x)
+    print(sum_nums)
 
 
 if __name__ == '__main__':
